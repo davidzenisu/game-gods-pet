a5244a887fc160391a3a98183a1deceaa9fdc074
David Zeni-Su <david@zeni-su.com>
2025-12-29 11:56:51 +0100

    Fix voting system

--- assets/scripts/ui/vote_ui.gd
+++ assets/scripts/ui/vote_ui.gd
@@ -14,38 +14,43 @@ func _on_round_started():
 func _process(delta):
 	if !self.visible or !multiplayer.is_server():
 		return
-	if Input.is_action_just_pressed("button_square0"):
-		GameManager.vote_cast(0,0)
-	if Input.is_action_just_pressed("button_square1"):
-		GameManager.vote_cast(0,1)
-	if Input.is_action_just_pressed("button_square2"):
-		GameManager.vote_cast(0,2)
-	if Input.is_action_just_pressed("button_square3"):
-		GameManager.vote_cast(0,3)
+	# Triangle: Player 1	
 	if Input.is_action_just_pressed("button_triangle0"):
-		GameManager.vote_cast(1,0)
+		GameManager.vote_cast(0,0)
 	if Input.is_action_just_pressed("button_triangle1"):
-		GameManager.vote_cast(1,1)
+		GameManager.vote_cast(0,1)
 	if Input.is_action_just_pressed("button_triangle2"):
-		GameManager.vote_cast(1,2)
+		GameManager.vote_cast(0,2)
 	if Input.is_action_just_pressed("button_triangle3"):
+		GameManager.vote_cast(0,3)
+	# Square: Player 2
+	if Input.is_action_just_pressed("button_square0"):
+		GameManager.vote_cast(1,0)
+	if Input.is_action_just_pressed("button_square1"):
+		GameManager.vote_cast(1,1)
+	if Input.is_action_just_pressed("button_square2"):
+		GameManager.vote_cast(1,2)
+	if Input.is_action_just_pressed("button_square3"):
 		GameManager.vote_cast(1,3)
-	if Input.is_action_just_pressed("button_cross0"):
-		GameManager.vote_cast(2,0)
-	if Input.is_action_just_pressed("button_cross1"):
-		GameManager.vote_cast(2,1)
-	if Input.is_action_just_pressed("button_cross2"):
-		GameManager.vote_cast(2,2)
-	if Input.is_action_just_pressed("button_cross3"):
-		GameManager.vote_cast(2,3)
+	# Circle: Player 3
 	if Input.is_action_just_pressed("button_circle0"):
-		GameManager.vote_cast(3,0)
+		GameManager.vote_cast(2,0)
 	if Input.is_action_just_pressed("button_circle1"):
-		GameManager.vote_cast(3,1)
+		GameManager.vote_cast(2,1)
 	if Input.is_action_just_pressed("button_circle2"):
-		GameManager.vote_cast(3,2)
+		GameManager.vote_cast(2,2)
 	if Input.is_action_just_pressed("button_circle3"):
+		GameManager.vote_cast(2,3)
+	# Cross: Player 4
+	if Input.is_action_just_pressed("button_cross0"):
+		GameManager.vote_cast(3,0)
+	if Input.is_action_just_pressed("button_cross1"):
+		GameManager.vote_cast(3,1)
+	if Input.is_action_just_pressed("button_cross2"):
+		GameManager.vote_cast(3,2)
+	if Input.is_action_just_pressed("button_cross3"):
 		GameManager.vote_cast(3,3)
+
 	#restart game on start button
 	if Input.is_action_just_pressed("button_start0"):
 		GameManager.restart_round()
