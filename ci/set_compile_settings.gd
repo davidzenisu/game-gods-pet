a1741ee8fd347c59047c8b1fbefd08216b26ca88
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2026-03-08 14:33:16 +0000

    Update script

--- ci/set_compile_settings.gd
+++ ci/set_compile_settings.gd
@@ -5,4 +5,7 @@ func _ready() -> void:
 	print("Set editor settings required for build!")
 	var settings = EditorInterface.get_editor_settings()
 	var java_sdk_path = OS.get_environment("JAVA_HOME")
+	print("Setting JAVA_HOME for the Java SDK path: ", java_sdk_path)
 	settings.set_setting("export/android/java_sdk_path", java_sdk_path)
+	print("Changed setting")
+	get_tree().quit()
