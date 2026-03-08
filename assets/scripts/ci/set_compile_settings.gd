5512c824b530e63b1e629a67efd4279e28cf0b44
David Zeni-Su <david@zeni-su.com>
2026-03-08 15:09:09 +0100

    Add script to set editor settings

---
+++ assets/scripts/ci/set_compile_settings.gd
@@ -0,0 +1,8 @@
+@tool
+extends EditorScript
+
+func _run() -> void:
+	print("Set editor settings required for build!")
+	var settings = EditorInterface.get_editor_settings()
+	var java_sdk_path = OS.get_environment("JAVA_HOME")
+	settings.set_setting("export/android/java_sdk_path", java_sdk_path)
