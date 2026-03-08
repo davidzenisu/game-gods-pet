15d2c91897cbda2914e3e955d176367bb59d0dce
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2026-03-08 13:19:57 +0000

    Update devcontainer config

--- .devcontainer/install_godot.sh
+++ .devcontainer/install_godot.sh
@@ -6,7 +6,7 @@ mkdir ~/.cache
 mkdir -p ~/.config/godot
 mkdir -p ~/.local/share/godot/export_templates/${GODOT_VERSION}.stable
 unzip Godot_v${GODOT_VERSION}-stable_linux.x86_64.zip
-mv Godot_v${GODOT_VERSION}-stable_linux.x86_64 /usr/local/bin/godot
+sudo mv Godot_v${GODOT_VERSION}-stable_linux.x86_64 /usr/local/bin/godot
 unzip Godot_v${GODOT_VERSION}-stable_export_templates.tpz
 mv templates/* ~/.local/share/godot/export_templates/${GODOT_VERSION}.stable
 rm -f Godot_v${GODOT_VERSION}-stable_export_templates.tpz Godot_v${GODOT_VERSION}-stable_linux.x86_64.zip
